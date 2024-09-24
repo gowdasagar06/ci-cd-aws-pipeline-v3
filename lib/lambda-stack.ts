d41168545c60458662303ef80e00dac4f730b10c
gowdasagar06 <sagargowda6666@gmail.com>
2024-09-24 10:47:02 +0530

    push

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import { Function, InlineCode, Runtime, Code} from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
@@ -13,53 +15,77 @@ export class MyLambdaStack extends cdk.Stack {
       //   code: Code.fromAsset(path.join(__dirname, 'lambda')), //resolving to ./lambda directory
       //   environment: { "stageName": stageName } //inputting stagename
       // });
-   const codeDeployRole = new iam.Role(this, 'CodeDeployRole', {
-        roleName: `${this.stackName}-code-deploy-role`, // Add stack name to the role name
-        assumedBy: new iam.ServicePrincipal('codedeploy.amazonaws.com'),
-        inlinePolicies: {
-          CodeDeployPermissions: new iam.PolicyDocument({
-            statements: [
-              new iam.PolicyStatement({
-                actions: [
-                  "ec2:Describe*",
-                  "s3:Get*",
-                  "s3:List*",
-                  "autoscaling:CompleteLifecycleAction",
-                  "autoscaling:DeleteLifecycleHook",
-                  "autoscaling:PutInstanceInStandby",
-                  "autoscaling:PutLifecycleHook",
-                  "autoscaling:RecordLifecycleActionHeartbeat",
-                  "autoscaling:ResumeProcesses",
-                  "autoscaling:SuspendProcesses",
-                  "autoscaling:TerminateInstanceInAutoScalingGroup",
-                  "cloudwatch:DescribeAlarms",
-                  "cloudwatch:PutMetricAlarm",
-                  "cloudwatch:DeleteAlarms",
-                  "cloudwatch:GetMetricStatistics",
-                  "cloudformation:DescribeStacks",
-                  "cloudformation:ListStackResources",
-                  "cloudformation:DescribeStackResources",
-                  "sns:Publish",
-                  "sns:ListTopics",
-                  "sns:GetTopicAttributes",
-                  "lambda:ListFunctions",
-                  "lambda:GetFunctionConfiguration",
-                  "ecs:DescribeServices",
-                  "ecs:DescribeTaskDefinition",
-                  "ecs:DescribeTasks",
-                  "ecs:ListTasks",
-                  "ecs:RegisterTaskDefinition",
-                  "ecs:UpdateService",
-                  "iam:PassRole"
-                ],
-                resources: ["*"],
-              }),
-            ],
-          }),
-        },
-        // environment: { "stageName": stageName }
-      });
+  //  const codeDeployRole = new iam.Role(this, 'CodeDeployRole', {
+  //       roleName: `${this.stackName}-code-deploy-role`, // Add stack name to the role name
+  //       assumedBy: new iam.ServicePrincipal('codedeploy.amazonaws.com'),
+  //       inlinePolicies: {
+  //         CodeDeployPermissions: new iam.PolicyDocument({
+  //           statements: [
+  //             new iam.PolicyStatement({
+  //               actions: [
+  //                 "ec2:Describe*",
+  //                 "s3:Get*",
+  //                 "s3:List*",
+  //                 "autoscaling:CompleteLifecycleAction",
+  //                 "autoscaling:DeleteLifecycleHook",
+  //                 "autoscaling:PutInstanceInStandby",
+  //                 "autoscaling:PutLifecycleHook",
+  //                 "autoscaling:RecordLifecycleActionHeartbeat",
+  //                 "autoscaling:ResumeProcesses",
+  //                 "autoscaling:SuspendProcesses",
+  //                 "autoscaling:TerminateInstanceInAutoScalingGroup",
+  //                 "cloudwatch:DescribeAlarms",
+  //                 "cloudwatch:PutMetricAlarm",
+  //                 "cloudwatch:DeleteAlarms",
+  //                 "cloudwatch:GetMetricStatistics",
+  //                 "cloudformation:DescribeStacks",
+  //                 "cloudformation:ListStackResources",
+  //                 "cloudformation:DescribeStackResources",
+  //                 "sns:Publish",
+  //                 "sns:ListTopics",
+  //                 "sns:GetTopicAttributes",
+  //                 "lambda:ListFunctions",
+  //                 "lambda:GetFunctionConfiguration",
+  //                 "ecs:DescribeServices",
+  //                 "ecs:DescribeTaskDefinition",
+  //                 "ecs:DescribeTasks",
+  //                 "ecs:ListTasks",
+  //                 "ecs:RegisterTaskDefinition",
+  //                 "ecs:UpdateService",
+  //                 "iam:PassRole"
+  //               ],
+  //               resources: ["*"],
+  //             }),
+  //           ],
+  //         }),
+  //       },
+  //       // environment: { "stageName": stageName }
+  //     });
+        
+  const lambdaRole = new iam.Role(this, 'LambdaServiceRole', {
+    assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    managedPolicies: [
+      iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+    ],
+  });
 
+  // Lambda function
+  const helloWorldLambda = new lambda.Function(this, 'HelloWorldLambda', {
+    functionName: 'HelloWorldFunction',
+    runtime: lambda.Runtime.NODEJS_16_X, // or any supported runtime
+    code: lambda.Code.fromInline(`
+      exports.handler = async (event) => {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify('Hello, world!'),
+        };
+        return response;
+      };
+    `),
+    handler: 'index.handler',
+    role: lambdaRole,
+  });
+  environment: { "stageName": stageName }
         
     }
     
