25b07d64b29edf55557612bab3164d037f71f941
gowdasagar06 <sagargowda6666@gmail.com>
2024-09-24 11:55:23 +0530

    push

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -4,6 +4,10 @@ import { Function, InlineCode, Runtime, Code} from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
+import { config } from './config';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
 
 
 export class MyLambdaStack extends cdk.Stack {
@@ -15,7 +19,23 @@ export class MyLambdaStack extends cdk.Stack {
       //   code: Code.fromAsset(path.join(__dirname, 'lambda')), //resolving to ./lambda directory
       //   environment: { "stageName": stageName } //inputting stagename
       // });
-   const codeDeployRole = new iam.Role(this, 'CodeDeployRole', {
+      const codePipelineBucket = new s3.Bucket(this, 'CodePipelineBucket', {
+        // bucketName: `${this.stackName}-lab`,
+        lifecycleRules: [{
+          id: config.bucketLifecyclePolicy.id,
+          enabled: config.bucketLifecyclePolicy.status === 'Enabled',
+          prefix: config.bucketLifecyclePolicy.prefix,
+          transitions: [{
+            storageClass: s3.StorageClass.INTELLIGENT_TIERING,
+            transitionAfter: cdk.Duration.days(config.bucketLifecyclePolicy.transitionInDays),
+          }],
+          expiration: cdk.Duration.days(config.bucketLifecyclePolicy.expirationInDays),
+        }],
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      });
+  
+      // Create CodeDeploy Role with stack name in the role name
+       const codeDeployRole = new iam.Role(this, 'CodeDeployRole', {
         roleName: `${this.stackName}-code-deploy-role`, // Add stack name to the role name
         assumedBy: new iam.ServicePrincipal('codedeploy.amazonaws.com'),
         inlinePolicies: {
@@ -59,11 +79,38 @@ export class MyLambdaStack extends cdk.Stack {
             ],
           }),
         },
-        // environment: { "stageName": stageName }
       });
-        
-        
-    }
-    
-    
+  
+      // Create CodeDeploy Application with stack name in the application name
+      const codeDeployApplication = new codedeploy.CfnApplication(this, 'CodeDeployApplication', {
+        applicationName: `${this.stackName}-application`,
+        computePlatform: 'Server',
+      });
+  
+      // Create CodeDeploy Deployment Group with stack name in the deployment group name
+      new codedeploy.CfnDeploymentGroup(this, 'CodeDeployDeploymentGroup', {
+        applicationName: codeDeployApplication.applicationName || `${this.stackName}-application`,
+        deploymentGroupName: `${this.stackName}-deploygroup`,
+        serviceRoleArn: codeDeployRole.roleArn,
+        deploymentConfigName: 'CodeDeployDefault.AllAtOnce',
+        ec2TagFilters: [
+          {
+            key: 'Name',
+            value: 'matson', // Replace with your EC2 tag
+            type: 'KEY_AND_VALUE',
+          },
+        ],
+        autoRollbackConfiguration: {
+          enabled: true,
+          events: [
+            'DEPLOYMENT_FAILURE',
+          ],
+        },
+        deploymentStyle: {
+          deploymentType: 'IN_PLACE',
+          deploymentOption: 'WITHOUT_TRAFFIC_CONTROL',
+        },
+      });
+  
+    }    
 }
